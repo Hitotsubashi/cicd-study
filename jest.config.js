d1786742302d0e81690d20b69fec11083febcd83
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-03-19 13:06:30 +0800

    remove react-app-polyfill

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
       },
     },
   },
-  setupFiles: ['react-app-polyfill/jsdom'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testEnvironment: 'jsdom',
