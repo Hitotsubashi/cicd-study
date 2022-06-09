782ca8cc93aed5b2f9be6e121dac26434e593970
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-09 20:58:58 +0800

    change test rollback.yml

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.5" />
+    <App value="1.3.6" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
