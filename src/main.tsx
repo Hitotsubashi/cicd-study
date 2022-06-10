76e169b097131b04654fec3820ced7372712e55e
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-10 11:43:55 +0800

    add with

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.8" />
+    <App value="1.3.9" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
