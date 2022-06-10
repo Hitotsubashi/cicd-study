d883a0c7ecec1e7e5cce3073dd1ce44f1eb26685
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-10 11:40:11 +0800

    try fetch tag

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.7" />
+    <App value="1.3.8" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
