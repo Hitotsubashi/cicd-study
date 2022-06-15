44cfc52cdc4522731501ae3ae9403590b557c993
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-15 20:25:23 +0800

    remove unuse file

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.14" />
+    <App value="1.3.15" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
