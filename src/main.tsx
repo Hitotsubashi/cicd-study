90310ba93f36775b1fb5ed04192a1780b9e5ce51
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-15 20:13:15 +0800

    change version

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.13" />
+    <App value="1.3.14" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
