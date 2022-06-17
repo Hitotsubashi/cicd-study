af353a7641afb4078e3ac45df852769087dbd033
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-17 17:05:52 +0800

    更新版本以及镜像源

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App value="1.3.16" />
+    <App value="1.3.17" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
