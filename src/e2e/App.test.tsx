6b64a5549a029c206bf2da57ffa6b0169457e22a
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-06-21 19:48:43 +0800

    add e2eTest

--- src/e2e/App.test.tsx
+++ src/e2e/App.test.tsx
@@ -1,9 +1,20 @@
+const DEFAULT_URL = 'http://localhost:3000/';
+
 describe('Check Page', () => {
   beforeAll(async () => {
-    await page.goto('http://localhost:3000/');
+    let dynamicUrl = '';
+    process.argv.forEach((item) => {
+      const matches = item.match(/^\-\-URL\=(.*)$/);
+      if (matches) {
+        dynamicUrl = matches[1];
+      }
+    });
+    await page.goto(dynamicUrl || DEFAULT_URL);
   });
 
-  it('should be titled "Google"', async () => {
-    await expect(page.title()).resolves.toMatch('Vite App');
+  it('click button', async () => {
+    await page.click('[role="button"]');
+    const content = await page.$eval('[role="button"]', (el) => el.textContent);
+    await expect(content).toEqual('count is: 1');
   });
 });
