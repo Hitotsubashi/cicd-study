841ebd59dc23407b05ddbe1c9cdee78aa2a72a2a
Calm Andy <thought7878@qq.com>
2023-11-08 19:22:19 +0800

    feat: update 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import { FC, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { FC, useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
-interface Props{
+interface Props {
   value: string;
 }
 
-const App:FC<Props> = ({value}) => {
-  const [count, setCount] = useState(0)
+const App: FC<Props> = ({ value }) => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>2</p>
         <p>
-          <button role="button" type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            role="button"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
           </button>
         </p>
         <p role="props">{value}</p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
